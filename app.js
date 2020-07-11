905bab6437efd65ef39955c390c4918ba954789d
Duany Dreyton <duanyrf@gmail.com>
2020-07-11 18:10:35 -0300

    Ajusta configurações para deploy no Heroku

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: `http://localhost:${process.env.PORT_FRONTEND}`,
+    origin: process.env.ADDR_FRONTEND,
   })
 );
 
